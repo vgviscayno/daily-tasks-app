06c79409eac1e13f85b10c41a2b2d46296d3e78b
vonrie <vonrie@lanturn.com>
2023-01-23 12:01:40 +0800

    update: modified styles for MainLayout & Configure Task window dimensions

--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -10,12 +10,12 @@ export default function MainLayout({ children }: MainLayoutProps) {
   return (
     <main className="flex">
       <nav className="border-r-2 w-4/12 p-4">
-        <div className="flex items-center border-solid border-2 p-4">
+        <div className="flex justify-center border-solid border-2 p-4">
           <h1>Daily Tasks</h1>
           <button
-            className="ml-auto"
+            className="ml-auto text-2xl font-semibold text-sky-400"
             onClick={() => {
-              const windowFeatures = "left=600,top=300,width=320,height=320";
+              const windowFeatures = "left=600,top=300,width=500,height=380";
               const handle = window.open(
                 "/tasks/new",
                 "newTaskWindow",
@@ -32,6 +32,7 @@ export default function MainLayout({ children }: MainLayoutProps) {
           </button>
         </div>
         <ul>
+          {/* load tasks here */}
           <li></li>
         </ul>
       </nav>
